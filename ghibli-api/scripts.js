67b6e4459352e480aa9b21aedca92234d1ff7fd0
L-alam <lalam@bu.edu>
2024-05-23 18:00:36 -0400

    setup Api

---
+++ ghibli-api/scripts.js
@@ -0,0 +1,24 @@
+// var request = new XMLHttpRequest()
+
+// request.open('GET', 'https://ghibli.rest/films', true)
+// request.onload = function () {
+//   // Begin accessing JSON data here
+//   var data = JSON.parse(this.response)
+
+//   if (request.status >= 200 && request.status < 400) {
+//     data.forEach((movie) => {
+//     })
+//   } else {
+//     console.log('error')
+//   }
+// }
+
+// request.send()
+
+// function getData() {
+//     const response = await fetch('https://ghibli.rest/films')
+//     const data = await response.json()
+//   }
+
+const app = document.getElementById('root')
+console.log(app)
